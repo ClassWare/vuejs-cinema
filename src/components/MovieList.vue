72c6e0c3efa234fdcc0a5da139c0d8f6f6aac464
martin <m.bazan@ntlworld.com>
2018-02-21 15:25:37 +0000

    feat after refactoring into single file components

---
+++ src/components/MovieList.vue
@@ -0,0 +1,39 @@
+<template>
+<div id="movie-list">
+   <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
+</div>
+</template>
+
+<script>
+  import genres from '../util/genres';
+
+  export default  {
+    data() {
+      return {
+        movies: [
+          { title: 'Pulp Fiction', genre:genres.CRIME },
+          { title: 'Home Alone', genre:genres.COMEDY },
+          { title: 'Quantum Of Solice', genre:genres.DRAMA },
+          { title: 'Taken II', genre:genres.CRIME },
+          { title: 'Austin Powers', genre:genres.COMEDY }
+        ]
+      }
+    },
+    props: [ 'genre', 'time' ],
+    methods: {
+      moviePassesGenreFilter(movie) {
+        if (this.genre.length) {
+          return this.genre.find(genre => movie.genre == genre);
+        } else {
+          return true;
+        }
+      }
+    },
+    computed: {
+      filteredMovies() {
+        return this.movies.filter(this.moviePassesGenreFilter);
+      }
+    }
+  }
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import './style.scss';
-import genres from './util/genres';
+
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
 
 new Vue({
   el: '#app',
@@ -18,82 +20,10 @@ new Vue({
           this[category].splice(i,1);
         }
       }
-      // console.log("PAYLOAD ", category,title,checked);
     }
   },
   components: {
-    'movie-list': {
-      template: `<div id="movie-list">
-                   <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
-                 </div>`,
-      data() {
-        return {
-          movies: [
-            { title: 'Pulp Fiction', genre:genres.CRIME },
-            { title: 'Home Alone', genre:genres.COMEDY },
-            { title: 'Quantum Of Solice', genre:genres.DRAMA },
-            { title: 'Taken II', genre:genres.CRIME },
-            { title: 'Austin Powers', genre:genres.COMEDY }
-          ]
-        }
-      },
-      props: [ 'genre', 'time' ],
-      methods: {
-        moviePassesGenreFilter(movie) {
-          if (this.genre.length) {
-            return this.genre.find(genre => movie.genre == genre);
-          } else {
-            return true;
-          }
-        }
-      },
-      computed: {
-        filteredMovies() {
-          return this.movies.filter(this.moviePassesGenreFilter);
-        }
-      }
-    },
-    'movie-filter': {
-      data() {
-        return {
-          genres
-        }
-      },
-      template: `<div id="movie-filter">
-                   <h2>Filter results</h2>
-                   <div class="filter-group">
-                     <check-filter
-                        v-for="genre in genres" v-bind:title="genre"
-                        v-on:check-filter="checkFilter">
-                     </check-filter>
-                   </div>
-                 </div>`,
-      methods: {
-        checkFilter(category,title,checked) {
-          this.$emit('check-filter',category,title,checked);
-        }
-      },
-      components: {
-        'check-filter': {
-          data() {
-            return {
-              checked: false
-            }
-          },
-          props: ['title'],
-          template: `<div v-bind:class="{ 'check-filter':true, active: checked }"
-                      @click="checkFilter">
-                      <span class="checkbox"></span>
-                      <span class="check-filter-title">{{ title }}</span>
-                    </div>`,
-          methods: {
-            checkFilter() {
-              this.checked = !this.checked;
-              this.$emit('check-filter','genre',this.title,this.checked);
-            }
-          }
-        }
-      }
-    }
+    MovieList,
+    MovieFilter
   }
 })
