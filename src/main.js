511639cc7802b2e07f4be95c5bef270792efddf4
martin <m.bazan@ntlworld.com>
2018-02-23 15:28:03 +0000

    feat completed project 2 built to run from production dist

--- src/main.js
+++ src/main.js
@@ -39,3 +39,37 @@ new Vue({
   },
   router
 })
+
+import { addClass, removeClass } from './util/helpers';
+
+let mouseoverHandler = function(ev) {
+  let span = ev.target.parentNode.getElementsByTagName('SPAN')[0];
+  addClass(span,'tooltip-show');
+};
+
+let mouseoutHandler = function(ev) {
+  let span = ev.target.parentNode.getElementsByTagName('SPAN')[0];
+  removeClass(span,'tooltip-show');
+};
+
+Vue.directive('tooltip', {
+  bind(el, bindings) {
+    let span = document.createElement('SPAN');
+    let text = document.createTextNode(`Seats available: ${ bindings.value.seats }`);
+    span.appendChild(text);
+    addClass(span, 'tooltip');
+    el.appendChild(span);
+    let div = el.getElementsByTagName('DIV')[0];
+    div.addEventListener('mouseover', mouseoverHandler);
+    div.addEventListener('mouseout', mouseoutHandler);
+    div.addEventListener('touchstart', mouseoverHandler);
+    div.addEventListener('touchend', mouseoutHandler);
+  },
+  unbind(el) {
+    let div = el.getElementsByTagName('DIV')[0];
+    div.removeEventListener('mouseover', mouseoverHandler);
+    div.removeEventListener('mouseout', mouseoutHandler);
+    div.removeEventListener('touchstart', mouseoverHandler);
+    div.removeEventListener('touchend', mouseoutHandler);
+  }
+})
